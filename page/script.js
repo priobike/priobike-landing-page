ace269955e090e508942145e089c02716cb4fb4d
adeveloper-wq <dajesch@gmail.com>
2024-04-17 07:44:05 +0200

    Fix Safari check

--- page/script.js
+++ page/script.js
@@ -1,13 +1,12 @@
-function isIOS() {
-  return navigator.userAgent.indexOf("iPhone") != -1;
-}
+var isIOS = navigator.userAgent.indexOf("like Mac") != -1;
 
-function isSafari() {
-  return navigator.userAgent.indexOf("Safari") != -1;
-}
+var isFirefox = navigator.userAgent.indexOf("FxiOS") != -1;
+var isChrome = navigator.userAgent.indexOf("CriOS") != -1;
+var isEdge = navigator.userAgent.indexOf("EdgiOS") != -1;
+var isOpera = navigator.userAgent.indexOf("OPT") != -1;
 
 if (isIOS()) {
-  if (!isSafari()) {
+  if (isFirefox || isChrome || isEdge || isOpera) {
     alert(
       "Um den Link in der PrioBike öffnen zu können, muss der Link auf Deinem iPhone mit Safari geöffnet werden."
     );
